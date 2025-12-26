8bdd5738a5af2db2acf3ece2a0430fa547bcb9d3
gaurav patel <81897751+gaurav0909-max@users.noreply.github.com>
2025-12-26 14:27:45 +0530

    Updated changes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Post,
+    Get,
+    UseGuards,
+} from '@nestjs/common';
+
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
+
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { RolesGuard } from '../common/guards/roles.guard';
 import { Role } from '../common/enums/role.enum';
+import { Roles } from 'src/common/decorators/roles.decorators';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) { }
+    constructor(private readonly authService: AuthService) { }
 
     @Post('login')
     async login(@Body() dto: LoginDto) {
@@ -23,5 +34,18 @@ export class AuthController {
     @Post('refresh')
     async refresh(@Body() dto: RefreshTokenDto) {
         return this.authService.refresh(dto.refreshToken);
+    }
+
+    /**
+     * RBAC proof endpoint
+     * Accessible only by OWNER role
+     */
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.OWNER)
+    @Get('protected')
+    protectedRoute() {
+        return {
+            message: 'Only OWNER can access this route',
+        };
     }
 }
